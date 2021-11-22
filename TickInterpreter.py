a46297e7efc708d5afa1db28cadc2a76b4e70f3e
Deep Thought <50927069+DeepThoughtt@users.noreply.github.com>
2021-11-22 03:00:48 +0100

    Add files via upload

---
+++ TickInterpreter.py
@@ -0,0 +1,30 @@
+class TickInterpreter:
+
+    def __init__(self, code):
+        self.code = code
+        self.tape = {0 : 0}
+        self.tptr = 0
+        
+    
+    def run(self):
+        out = ""
+        
+        for command in self.code:
+            if command == "+":
+                self.tape[self.tptr] = (self.tape[self.tptr] + 1) % 256
+            
+            if command == "*":
+                out += chr(self.tape[self.tptr])
+                
+            if command == "<":
+                self.tptr -= 1
+                if self.tptr not in self.tape.keys():
+                    self.tape[self.tptr] = 0
+                    
+            if command == ">":
+                self.tptr += 1
+                if self.tptr not in self.tape.keys():
+                    self.tape[self.tptr] = 0
+                    
+        return out
+
